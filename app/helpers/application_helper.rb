c63bb30132d6805abf97e84d54e9d4673c8d82bb
Ankur <anx450z@gmail.com>
2022-11-14 12:10:32 +0530

    completed static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = 'Learn Ruby'
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
