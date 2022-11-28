f1ca77c4447616d73a248b85b62f2ed6780db013
Ankur <anx450z@gmail.com>
2022-11-28 14:08:15 +0530

    add dropdown-menu

---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,10 @@
+// Menu manipulation
+// Add toggle listeners to listen for clicks.
+document.addEventListener("turbo:load", function () {
+  let account = document.querySelector("#account");
+  account.addEventListener("click", function (event) {
+    event.preventDefault();
+    let menu = document.querySelector("#dropdown-menu");
+    menu.classList.toggle("active");
+  });
+});
